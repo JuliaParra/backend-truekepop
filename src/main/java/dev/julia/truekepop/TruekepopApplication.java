309f5bb8073c3e251c279d6fe9193699fd333e2d
Julia <musicaparatodo770@gmail.com>
2024-09-15 20:08:14 +0200

    preparacion de proyecto

---
+++ src/main/java/dev/julia/truekepop/TruekepopApplication.java
@@ -0,0 +1,13 @@
+package dev.julia.truekepop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruekepopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruekepopApplication.class, args);
+	}
+
+}
